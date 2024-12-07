482eed89383347a297b511ef689a86abba856d44
Touhid <touhidul.swe@gmail.com>
2024-12-07 13:38:04 +0600

    Author added

--- book_lover/src/components/Blog/Blog.jsx
+++ book_lover/src/components/Blog/Blog.jsx
@@ -1,14 +1,21 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({blog}) => {
-   const {title} =blog;
+   const {title ,cover,author_img} =blog;
     return (
         <div>
-            <h2 className="4-xl">{title}</h2>
-        </div>
+    <h2 className="4-xl">{title}</h2>
+    <img src={author_img} alt="" className="w-12 h-12 " />
+    <img src={cover} alt={`Cover Pic Of The Title ${title}`} />
+</div>
+
     );
 };
-Blog.PropTypes = {
-    blog:PropTypes.object.isRequired
-}
+Blog.propTypes = {
+    blog: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        cover: PropTypes.string.isRequired,
+        author_img: PropTypes.string.isRequired, // Ensure that author_img is passed as a string
+    }).isRequired,
+};
 export default Blog;
--- book_lover/src/components/Header/Blogs/Blogs.jsx
+++ book_lover/src/components/Header/Blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Blog from '../../Blog/Blog';
 
 const Blogs = () => {
     const [blogs, setBlogs] =useState([]);
@@ -11,7 +12,10 @@ const Blogs = () => {
         <div className='md:w-2/3'>
             <h1 className="text-4xl">Blogs: {blogs.length}</h1>
             {
-                blogs.map(blog=> <Blog key={blog.id} blog={blog}></Blog>)
+                blogs.map(blog=> <Blog 
+                    key={blog.id} 
+                    blog={blog}>   
+                    </Blog>)
             }
         </div>
     );
