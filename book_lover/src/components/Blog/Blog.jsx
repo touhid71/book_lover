6e23b11a9393cbd7c6a791864ca8d92e999ee706
Touhid <touhidul.swe@gmail.com>
2024-12-09 11:37:16 +0600

    author data and reading time added

--- book_lover/src/components/Blog/Blog.jsx
+++ book_lover/src/components/Blog/Blog.jsx
@@ -1,14 +1,29 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const Blog = ({blog}) => {
-   const {title ,cover,author_img} =blog;
+const Blog = ({ blog }) => {
+    const { title, cover, author, author_img, reading_time,posted_date,hash_tag } = blog;
     return (
         <div>
-    <h2 className="4-xl">{title}</h2>
-    <img src={author_img} alt="" className="w-12 h-12 " />
-    <img src={cover} alt={`Cover Pic Of The Title ${title}`} />
-</div>
-
+            <img src={cover} alt={`Cover Pic Of The Title ${title}`} />
+            <div className="flex justify-between">
+                <div className="flex">
+                    <img className="w-14" src={author_img} alt="" />
+                    <div className="ml-6">
+                       <h3 className="2xl">{author}</h3> 
+                       <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time}Min Read</span>
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+            <p>
+               {
+                hash_tag.map((hash ,i) => <span key={i}><a href="">{hash}</a></span>)
+               }
+            </p>
+        </div>
     );
 };
 Blog.propTypes = {
