bb28c4953425d7088ad128403b89e64f21d06528
Touhid <touhidul.swe@gmail.com>
2024-12-13 19:07:16 +0600

    added notification

--- book_lover/src/App.jsx
+++ book_lover/src/App.jsx
@@ -6,6 +6,7 @@ import Header from "./components/Header/Header";
 
 function App() {
   const [bookmarks, setBookmarks] = useState([]);
+  const [notification, setNotification] = useState("");
 
   const handleAddToBookmark = (blog) => {
     const isAlreadyBookmarked = bookmarks.find((b) => b.id === blog.id);
@@ -13,10 +14,17 @@ function App() {
     if (isAlreadyBookmarked) {
       // If the blog is already bookmarked, remove it
       setBookmarks(bookmarks.filter((b) => b.id !== blog.id));
+      setNotification(`Removed from bookmarks: ${blog.title}`);
     } else {
       // Otherwise, add it to the bookmarks
       setBookmarks([...bookmarks, blog]);
+      setNotification(`Added to bookmarks: ${blog.title}`);
     }
+
+    // Automatically clear the notification after 3 seconds
+    setTimeout(() => {
+      setNotification("");
+    }, 3000);
   };
 
   return (
@@ -26,6 +34,13 @@ function App() {
         <Blogs handleAddToBookmark={handleAddToBookmark} />
         <Bookmarks bookmarks={bookmarks} />
       </div>
+
+      {/* Notification */}
+      {notification && (
+        <div className="notification fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-blue-500 text-white p-4 rounded-lg">
+          {notification}
+        </div>
+      )}
     </>
   );
 }
--- book_lover/src/components/Bookmarks/Bookmark.jsx
+++ book_lover/src/components/Bookmarks/Bookmark.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import PropTypes from "prop-types";
 
-const Bookmark = ({bookmarks}) => {
-    return (
-        <div className='md:w-1/3 '>
-            
-            <h2>Bookmark: {bookmarks.length}</h2>
+const Bookmarks = ({ bookmarks, notification }) => {
+  return (
+    <div className="md:w-1/3">
+      <h1 className="text-4xl">Bookmarked Blogs</h1>
+      
+      {/* Display Notification */}
+      {notification && (
+        <div className="notification bg-blue-500 text-white p-4 rounded-lg my-4">
+          {notification}
         </div>
-    );
+      )}
+
+      {/* Display List of Bookmarked Blogs */}
+      <ul>
+        {bookmarks.map((blog) => (
+          <li key={blog.id} className="border-b-2 p-2">
+            <h3>{blog.title}</h3>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
-export default Bookmark;
+Bookmarks.propTypes = {
+  bookmarks: PropTypes.array.isRequired,
+  notification: PropTypes.string,
+};
+
+export default Bookmarks;
