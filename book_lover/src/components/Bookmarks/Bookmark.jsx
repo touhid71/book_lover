b90f7f8355296803159ad2caac7e5e2a0acae1d0
Touhid <touhidul.swe@gmail.com>
2024-12-18 13:50:00 +0600

    Added Bookmark

--- book_lover/src/components/Bookmarks/Bookmark.jsx
+++ book_lover/src/components/Bookmarks/Bookmark.jsx
@@ -1,38 +1,61 @@
 import PropTypes from "prop-types";
 
 const Bookmarks = ({ bookmarks, notification }) => {
-  // Determine the background color based on the notification type
+  // Determine the background color for notifications
   const notificationBackground =
     notification.type === "add" ? "#006400" : "#FF7F00"; // Deep green or orange
 
   return (
-    <div className="md:w-1/3">
-      <h1 className="text-4xl">Bookmarked</h1>
-      
-      {/* Display Notification with animation */}
-      {notification.message && (
-        <div
-          className={`notification p-4 rounded-lg my-4 ${notification.visible ? 'visible' : ''}`}
-          style={{ backgroundColor: notificationBackground }}
-        >
-          {notification.message}
+    <div className="md:w-1/3 ml-4 mt-11 pt-3">
+      {bookmarks.length > 0 && (
+        <div className="bg-gray-300 rounded-md pb-1">
+          <h1 className="text-lg pl-9 bg-gradient-to-r from-gray-600 to-green-600 text-white pt-1 pb-1 rounded-t-md">
+            Bookmarked Blogs: {bookmarks.length}
+          </h1>
+
+          {/* Display Notification */}
+          {notification.message && (
+            <div
+              className="notification p-4 rounded-lg my-4"
+              style={{ backgroundColor: notificationBackground }}
+            >
+              {notification.message}
+            </div>
+          )}
+
+          {/* Display List of Bookmarked Blogs */}
+          <div className="m-2">
+            {bookmarks.map((blog) => (
+              <div
+                key={blog.id}
+                className={`p-2 mt-2 rounded-md ${
+                  blog.isNew ? "bg-green-200" : "bg-slate-50"
+                }`}
+              >
+                <h3>{blog.title}</h3>
+              </div>
+            ))}
+          </div>
         </div>
       )}
 
-      {/* Display List of Bookmarked Blogs */}
-      <ul>
-        {bookmarks.map((blog) => (
-          <li key={blog.id} className="border-b-2 p-2">
-            <h3>{blog.title}</h3>
-          </li>
-        ))}
-      </ul>
+      {bookmarks.length === 0 && (
+       <h1 className="text-lg pl-9 bg-gradient-to-r from-gray-600 to-green-600 text-white pt-1 pb-1">
+       Bookmarked Blogs: {bookmarks.length}
+     </h1>
+      )}
     </div>
   );
 };
 
 Bookmarks.propTypes = {
-  bookmarks: PropTypes.array.isRequired,
+  bookmarks: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      isNew: PropTypes.bool, // Field to determine if the item is new
+    })
+  ).isRequired,
   notification: PropTypes.shape({
     message: PropTypes.string,
     type: PropTypes.string,
