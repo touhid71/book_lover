096eef455f1f094bb92b1ecbb42a9584a1e1c657
Touhid <touhidul.swe@gmail.com>
2024-12-19 16:20:54 +0600

    Added menu

--- book_lover/src/components/Header/Header.jsx
+++ book_lover/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import logo from '../../assets/images/logo.png'
 
 const Header = () => {
     return (
-        <div className='flex justify-between items-center p-4 max-w-4xl mx-auto border-b-2'>
+        <div className='flex justify-between items-center p-4 max-w-4xl mx-auto border-b-2 header'>
             <h1>
                 <img 
                     src={logo}
