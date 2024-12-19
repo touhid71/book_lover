4af957ffe0ea92f4bcb035a40c8d48e6edfc05d8
Touhid <touhidul.swe@gmail.com>
2024-12-19 14:28:43 +0600

    Added Logo

--- book_lover/src/components/Header/Header.jsx
+++ book_lover/src/components/Header/Header.jsx
@@ -1,9 +1,21 @@
-import profile from '../../assets/images/profile.png'
+import profile from '../../assets/images/profile.png';
+import logo from '../../assets/images/logo.png'
+
 const Header = () => {
     return (
         <div className='flex justify-between items-center p-4 max-w-4xl mx-auto border-b-2'>
-            <h1 className='text-4xl font-bold'>Book Lover</h1>
-            <img src={profile} alt="" />
+            <h1>
+                <img 
+                    src={logo}
+                    alt="Logo" 
+                    className="h-16 w-16" // Adjust logo size to 4rem (64px)
+                />
+            </h1>
+            <img 
+                src={profile} 
+                alt="Profile" 
+                className="h-16 w-16 rounded-full" // Adjust profile image size to 4rem (64px)
+            />
         </div>
     );
 };
