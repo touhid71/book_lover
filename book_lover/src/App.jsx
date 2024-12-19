8cdac0caf5b12f686a13142e089d5547b7c6aefc
Touhid <touhidul.swe@gmail.com>
2024-12-19 12:48:15 +0600

    added css

--- book_lover/src/App.jsx
+++ book_lover/src/App.jsx
@@ -6,7 +6,7 @@ import Header from "./components/Header/Header";
 
 function App() {
   const [bookmarks, setBookmarks] = useState([]);
-  const [notification, setNotification] = useState(""); // State for notification
+  const [notification, setNotification] = useState(null); // State for notification
 
   const handleAddToBookmark = (blog) => {
     const isAlreadyBookmarked = bookmarks.find((b) => b.id === blog.id);
@@ -14,17 +14,23 @@ function App() {
     if (isAlreadyBookmarked) {
       // If the blog is already bookmarked, remove it
       setBookmarks(bookmarks.filter((b) => b.id !== blog.id));
-      setNotification(`Removed from bookmarks.`);
+      setNotification({
+        message: `Removed from bookmarks.`,
+        type: 'remove', // Set 'remove' type
+      });
     } else {
       // Otherwise, add it to the bookmarks
       setBookmarks([...bookmarks, blog]);
-      setNotification(`Added to bookmarks.`);
+      setNotification({
+        message: `Added to bookmarks.`,
+        type: 'add', // Set 'add' type
+      });
     }
 
-    // Clear the notification after 4 seconds (matching the CSS animation time)
+    // Clear the notification after 3 seconds
     setTimeout(() => {
-      setNotification("");
-    }, 4000);
+      setNotification(null); // Clear notification when done
+    }, 3000);
   };
 
   return (
@@ -32,13 +38,13 @@ function App() {
       <Header />
       {/* Notification */}
       {notification && (
-        <div className="notification">
-          {notification}
+        <div className={`notification ${notification.type}`}>
+          {notification.message}
         </div>
       )}
       <div className="md:flex max-w-4xl mx-auto">
         <Blogs handleAddToBookmark={handleAddToBookmark} />
-        <Bookmarks bookmarks={bookmarks} notification={notification} />
+        <Bookmarks bookmarks={bookmarks} />
       </div>
     </>
   );
